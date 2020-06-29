db143ce33f7a59fee35e22a35f491a0dda19d666
Eleanor Chodroff <eleanor.chodroff@gmail.com>
2020-06-29 15:41:28 +0100

    spacing

--- phonetic_measures/local_extract/MultitaperSpectralMomentsPeak.R
+++ phonetic_measures/local_extract/MultitaperSpectralMomentsPeak.R
@@ -18,7 +18,7 @@
 # - there is a directory with two sub-directories: extracts (maindir) and sibmeasures (outdir) 
 # - the extracts directory contains sub-directories (languages) containing sibilant extracts with .wav extensions
 # - the extract filenames have the following structure ORIGFILENAME_SIBILANT*_00O*.wav 
-# --- the original filename (ORIGFILENAME) can be pretty much anything, the sibilant (or segment label) can be in mixed case, and there can be any amount of numbers preceding the .wav extension (this corresponded to its interval number in the TextGrid). It is more likely than not that you will need to modify the regex at the end of this script to work for your data. 
+# --- the original filename (ORIGFILENAME) can be pretty much anything, the sibilant (or segment label) can be in mixed case, and there can be any amount of numbers preceding the .wav extension (this corresponded to its interval number in the TextGrid). 
 
 
 #################
@@ -27,13 +27,11 @@
 
 maindir <- '/Users/xxx/sibilants/extracts/' # main directory with sub-folders of sound extracts
 outdir <- '/Users/xxx/sibilants/measures/' # output directory for text file
-subdirs <- c("ASDFGH", "LKJHGF") # sub-directories within maindir that contain sibilant extracts (these corresponded to language IDs for us)
+subdirs <- c('ASDFGH', 'LKJHGF') # sub-directories within maindir that contain sibilant extracts (these corresponded to language IDs for us)
 SAMPLE_RATE <- 16000 # sampling rate of extracts
 NW_TIME_BANDWIDTH <- 4 # time-bandwidth parameter for multitaper spectrum (nw)
 K_TAPERS <- 8 # number of tapers for multitaper spectrum (k)
 OUTFILE_EXT <- 'sibilants.csv' # extension to add to the output file (if you change csv, you'll need to change code below) 
-
-### HAVE YOU CHECKED THE REGEX AT THE END OF THE SCRIPT? ### 
 
 ################
 ### LET'S GO ###
@@ -49,9 +47,9 @@ require(stringr)
 
 # location can be "beg", "mid", or "end"
 getPortion <- function(frici, n_samples, fraction, location) {
-        percent_samples <- fraction*n_samples
+        percent_samples <- fraction * n_samples
         if (location == "mid") {
-        	edges <- (n_samples-percent_samples)/2
+        	edges <- (n_samples - percent_samples) / 2
         	start <- round(edges)
         	end <- round(n_samples-edges)
         } else if (location == "end") {
@@ -66,8 +64,8 @@ getPortion <- function(frici, n_samples, fraction, location) {
 }
 
 getSpectrum <- function(frici) {
-	xi <- ts(attributes(frici)$left, frequency=SAMPLE_RATE);
-    mti <- spec.mtm(xi, nw=NW_TIME_BANDWIDTH, k=K_TAPERS, plot=FALSE);
+	xi <- ts(attributes(frici)$left, frequency = SAMPLE_RATE)
+    mti <- spec.mtm(xi, nw = NW_TIME_BANDWIDTH, k = K_TAPERS, plot = FALSE)
 }
 
 getCOG <- function(mti) {
@@ -76,20 +74,20 @@ getCOG <- function(mti) {
 
 getVar <- function(mti) {
 	X <- (mti$spec / sum(mti$spec))
-    Y <- outer(cogi,mti$freq,function(x,y) { (y-x)^2 })
+    Y <- outer(cogi, mti$freq, function(x, y) { (y - x)^2 })
     X %*% Y
 }
 
 getSkew <- function(mti, spectral.vari) {
 	 X <- (mti$spec / sum(mti$spec))
-     Y <- outer(cogi,mti$freq,function(x,y) { (y-x)^3 })
+     Y <- outer(cogi,mti$freq, function(x, y) { (y - x)^3 })
      skewi <- X %*% Y
      skewi / spectral.vari^(3/2)
 }
 
 getKurt <- function(mti, spectral.vari) {
 	  X <- (mti$spec / sum(mti$spec))
-      Y <- outer(cogi,mti$freq,function(x,y) { (y-x)^4 })
+      Y <- outer(cogi, mti$freq, function(x, y) { (y - x)^4 })
       kurti <- X %*% Y
       (kurti / spectral.vari^2) - 3
 }
@@ -101,16 +99,16 @@ getSpectralPeak <- function(mti) {
 }
 
 getMidPeak <-function(mti, lowFreq, highFreq) {
-	lowend <- which(abs(mti$freq-lowFreq)==min(abs(mti$freq-lowFreq)))
-    highend <- which(abs(mti$freq-highFreq)==min(abs(mti$freq-highFreq)))
+	lowend <- which(abs(mti$freq - lowFreq) == min(abs(mti$freq - lowFreq)))
+    highend <- which(abs(mti$freq - highFreq) == min(abs(mti$freq - highFreq)))
     peakamp <- max(mti$spec[lowend:highend])
     loc <- which(mti$spec[lowend:highend] == peakamp)
     mti$freq[lowend:highend][loc]
 }
 
 substrRight <- function(x, n){
-    substr(x, nchar(x)-n+1, nchar(x))
-    }
+    substr(x, nchar(x) - n + 1, nchar(x))
+}
     
 ##################
 ### FILE LOOPS ###
@@ -120,10 +118,10 @@ for (j in 1:length(subdirs)) {
     # location of sound extracts
     lang <- subdirs[j]
     mydir <- paste0(mainDir, lang)
-    outfile <- paste(lang, OUTFILE_EXT, sep="_")
+    outfile <- paste(lang, OUTFILE_EXT, sep = "_")
 
     # create list of files and create empty vectors for each measure
-    files <- list.files(mydir, pattern="*.wav")
+    files <- list.files(mydir, pattern = "*.wav")
     cogs <- rep(NA, length(files))
     peaks <- rep(NA,length(files))
     peaks3k7k <- rep(NA,length(files))
@@ -133,8 +131,8 @@ for (j in 1:length(subdirs)) {
     kurts <- rep(NA,length(files))
     
     for (i in 1:length(files)) {
-        fi <- files[i];
-        frici <- readWave(paste(mydir,fi,sep="/"));
+        fi <- files[i]
+        frici <- readWave(paste(mydir,fi,sep="/"))
         frici <- normalize(frici)
         n_samples <- length(frici)
         
@@ -151,23 +149,8 @@ for (j in 1:length(subdirs)) {
         }
 
 	# set up output file
-    measures <- data.frame(stim=files, cog=cogs, peak=peaks, peak3k7k=peaks3k7k, peak2k6k=peaks2k6k, spectral.var=specvars, skew=skews, kurtosis=kurts)
+    measures <- data.frame(stim = files, cog = cogs, peak = peaks, peak3k7k = peaks3k7k, peak2k6k = peaks2k6k, spectral.var = specvars, skew = skews, kurtosis = kurts)
     
-	#################
-	### CHANGE ME ###
-	#################
- 
-	measures$lang <- lang
-	measures$file <- gsub("_[a-zA-Z~\`\\]*_[0-9]*.wav", "", measures$stim)
-	measures$file <- gsub("_[a-zA-Z]$", "", measures$file)
-	measures$sib <- str_remove(measures$stim, measures$file)
-	measures$sib <- gsub("_[0-9]*.wav", "", measures$sib)
-	measures$sib <- gsub("^_", "", measures$sib)
-	measures$trial <- str_remove(measures$stim, measures$file)
-	measures$trial <- str_extract(measures$trial, "[0-9]+")
-	measures$trial <- as.character(as.numeric(measures$trial))
-	measures$stim <- NULL
-
 	# save output
     write.csv(measures, paste0(outdir, outfile))
 }
